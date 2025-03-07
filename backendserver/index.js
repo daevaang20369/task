38656766b601f8ff810b8b972cde0a0d4ba278bc
daevaang20369 <Daevaang20369@iiitd.ac.in>
2025-03-07 12:07:01 +0530

    sdf

--- backendserver/index.js
+++ backendserver/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.get("/", (req, res) => {
     res.json({ message: "hi" });
 });
